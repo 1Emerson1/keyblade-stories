bdc0730f53bbcde2fb15d4abe62ee11d29016acb
Jmul340 <32146076+Jmul340@users.noreply.github.com>
2019-02-27 11:14:16 -0500

    Added hero component
    Added a hero component called hero-ing. Included the hero image within the src/assets folder. Image doesn't display the hero image, but does display the text.

---
+++ src/app/hero-ing/hero-ing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-ing',
+  templateUrl: './hero-ing.component.html',
+  styleUrls: ['./hero-ing.component.css']
+})
+export class HeroIngComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
